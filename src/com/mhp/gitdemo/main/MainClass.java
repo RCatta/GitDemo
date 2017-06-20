691b5912f993b4f1bca4ea16e9ada26e6b0dd840
Cata Visan <Cata.visan@soft-nova.com>
2017-06-20 11:27:56 +0300

    new content

--- src/com/mhp/gitdemo/main/MainClass.java
+++ src/com/mhp/gitdemo/main/MainClass.java
@@ -3,8 +3,8 @@ package com.mhp.gitdemo.main;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello world");
+		
 	}
 
 }
